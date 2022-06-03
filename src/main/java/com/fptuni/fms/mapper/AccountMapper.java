3483aa375d2036cfc4e8b02f01a61f7c270dc54e
An <htxgiacatan@gmail.com>
2022-06-03 11:33:22 +0700

    Create AccountMapper.java

---
+++ src/main/java/com/fptuni/fms/mapper/AccountMapper.java
@@ -0,0 +1,31 @@
+package com.fptuni.fms.mapper;
+
+import com.fptuni.fms.dao.implement.AbstractDAO;
+import com.fptuni.fms.model.Account;
+import com.fptuni.fms.model.Orders;
+import com.fptuni.fms.model.Role;
+import com.fptuni.fms.model.Store;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author NhatTan
+ */
+public class AccountMapper extends AbstractDAO<Orders> implements RowMapper {
+
+    @Override
+    public Account mapRow(ResultSet rs) {
+        Account acc = null;
+        try {
+            acc = new Account();
+            acc.setId(rs.getInt("ID"));
+            acc.setUsername(rs.getString("Username"));
+            acc.setFullName(rs.getString("FullName"));
+            acc.setRoleID(new Role(rs.getInt("RoleID")));
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return acc;
+    }
+}
