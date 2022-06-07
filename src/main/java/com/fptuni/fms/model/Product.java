910152166df391e7f173319d047f4b9c4f8d75c1
AnhQuoc2906 <105768556+AnhQuoc2906@users.noreply.github.com>
2022-06-07 18:31:21 +0700

    Add files via upload

---
+++ src/main/java/com/fptuni/fms/model/Product.java
@@ -0,0 +1,180 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.fptuni.fms.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author LucasBV
+ */
+@Entity
+@Table(name = "Product")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Product.findAll", query = "SELECT p FROM Product p"),
+    @NamedQuery(name = "Product.findById", query = "SELECT p FROM Product p WHERE p.id = :id"),
+    @NamedQuery(name = "Product.findByName", query = "SELECT p FROM Product p WHERE p.name = :name"),
+    @NamedQuery(name = "Product.findByUnit", query = "SELECT p FROM Product p WHERE p.unit = :unit"),
+    @NamedQuery(name = "Product.findByPrice", query = "SELECT p FROM Product p WHERE p.price = :price"),
+    @NamedQuery(name = "Product.findByQtyAvailable", query = "SELECT p FROM Product p WHERE p.qtyAvailable = :qtyAvailable")})
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "ID")
+    private String id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "Name")
+    private String name;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "Unit")
+    private String unit;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "Price")
+    private BigDecimal price;
+    @Column(name = "QtyAvailable")
+    private Short qtyAvailable;
+    @JoinColumn(name = "CateID", referencedColumnName = "ID")
+    @ManyToOne
+    private Category cateID;
+    @JoinColumn(name = "StoreID", referencedColumnName = "ID")
+    @ManyToOne(optional = false)
+    private Store storeID;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "product")
+    private List<OrderDetail> orderDetailList;
+
+    public Product() {
+    }
+
+    public Product(String id) {
+        this.id = id;
+    }
+
+    public Product(String id, String name, String unit, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.unit = unit;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Short getQtyAvailable() {
+        return qtyAvailable;
+    }
+
+    public void setQtyAvailable(Short qtyAvailable) {
+        this.qtyAvailable = qtyAvailable;
+    }
+
+    public Category getCateID() {
+        return cateID;
+    }
+
+    public void setCateID(Category cateID) {
+        this.cateID = cateID;
+    }
+
+    public Store getStoreID() {
+        return storeID;
+    }
+
+    public void setStoreID(Store storeID) {
+        this.storeID = storeID;
+    }
+
+    @XmlTransient
+    public List<OrderDetail> getOrderDetailList() {
+        return orderDetailList;
+    }
+
+    public void setOrderDetailList(List<OrderDetail> orderDetailList) {
+        this.orderDetailList = orderDetailList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Product)) {
+            return false;
+        }
+        Product other = (Product) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.fptuni.fms.model.Product[ id=" + id + " ]";
+    }
+
+}
---
+++ src/main/webapp/register.jsp
@@ -0,0 +1,76 @@
+<%-- 
+    Document   : index
+    Created on : May 31, 2022, 8:32:52 AM
+    Author     : Admin
+--%>
+
+    <%@page contentType="text/html" pageEncoding="UTF-8"%>
+        <!DOCTYPE html>
+        <html>
+
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <title>Register</title>
+            <link rel="stylesheet" type="text/css" href="CSS/css/bootstrap.css" />
+        </head>
+
+        <body style="background-color:#9EBF99">
+
+        <header class="text-center col-sm-4" style="margin-left:auto;margin-right:auto;">
+            <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="tab-login" data-mdb-toggle="pill" href="index.jsp" role="tab"
+                       aria-controls="pills-login" aria-selected="false">Login</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" id="tab-register" data-mdb-toggle="pill" href="register.jsp" role="tab"
+                       aria-controls="pills-register" aria-selected="true">Register</a>
+                </li>
+            </ul>
+        </header>
+        <section><div class="text-center">
+            <section class="btn btn-light" style="position:relative;">
+            <!-- Register buttons -->
+            <div class="text-center" style="width:400px;">
+                <p style="font-weight:bold;"> REGISTER </p>
+                <!-- Name input -->
+                <div class="form-outline mb-4 flex-fill" style="text-align:left;">
+                    <p>Name: <input type="text" id="registerName" placeholder="Name" style="width:100%;"/></p>
+                </div>
+
+                <!-- Username input -->
+                <div class="form-outline mb-4" style="text-align:left;">
+                    <p>Username: <input type="text" id="registerUsername" placeholder="Username" style="width:100%;"/></p>
+                </div>
+
+                <!-- Password input -->
+                <div class="form-outline mb-4" style="text-align:left;">
+                    <p>Password: <input type="password" id="registerPassword" placeholder="Password" style="width:100%;"/></p>
+                </div>
+
+                <!-- Repeat Password input -->
+                <div class="form-outline mb-4" style="text-align:left;">
+                    <p>Repeat Password: <input type="password" id="registerRepeatPassword" placeholder="Password" style="width:100%;"/></p>
+                </div>
+
+                <!-- Checkbox -->
+                <div class="form-check d-flex justify-content-center mb-4">
+                    <input class="form-check-input me-2" type="checkbox" value="" id="registerCheck"
+                           aria-describedby="registerCheckHelpText" />
+                    <label class="form-check-label" for="registerCheck">
+                        I have read and agree to the terms
+                    </label>
+                </div>
+
+                <!-- Submit button -->
+                <button type="submit" class="btn btn-primary btn-block mb-3">Sign in</button>
+                </form>
+                <p>Already a member? <a href="index.jsp">Login now</a></p>
+            </div>
+            </section></div>
+        </section>
+
+        <footer></footer>
+        </body>
+
+        </html>
