862ad97e295a327fc0d91398dbe58c62c6af9662
An <htxgiacatan@gmail.com>
2022-06-20 18:23:36 +0700

    Update IStoreDAO.java

--- src/main/java/com/fptuni/fms/dao/IStoreDAO.java
+++ src/main/java/com/fptuni/fms/dao/IStoreDAO.java
@@ -1,6 +1,8 @@
 package com.fptuni.fms.dao;
 
+import com.fptuni.fms.model.Account;
 import com.fptuni.fms.model.Store;
+import com.fptuni.fms.paging.Pageable;
 
 import java.util.List;
 
@@ -8,14 +10,20 @@ import java.util.List;
  *
  * @author Anh Quoc
  */
-
 public interface IStoreDAO extends GenericDAO<Store> {
 
 //    Store getStore(String name);
-
     List<Store> getStore();
+
+    Store getStore(int id);
 
     Integer insertStore(Store store);
 
-    void updateStore(int id, String name, int AccountID);
+    boolean updateStore(int id, String name, int AccountID);
+
+    List<Store> getListStore(Pageable pageable);
+
+    int count();
+
+    boolean Delete(int id);
 }
