c95e9ac56d3d775f57cf6dc0c090f41e219ffaaa
An <annnse150166@fpt.edu.vn>
2022-06-22 15:17:04 +0700

    Update AccountService.java

--- src/main/java/com/fptuni/fms/service/implement/AccountService.java
+++ src/main/java/com/fptuni/fms/service/implement/AccountService.java
@@ -114,7 +114,7 @@ public class AccountService implements IAccountService {
     @Override
     public String getListAccount(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int pageIndex = 1;
-        int pageSize = 10;
+        int pageSize = 6;
         String sortField = "ID";
         boolean isAsc = true;
         if (request.getParameter("page") != null) {
@@ -149,7 +149,7 @@ public class AccountService implements IAccountService {
     @Override
     public String search(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int pageIndex = 1;
-        int pageSize = 10;
+        int pageSize = 6;
         String sortField = "ID";
         boolean isAsc = true;
         if (request.getParameter("page") != null) {
