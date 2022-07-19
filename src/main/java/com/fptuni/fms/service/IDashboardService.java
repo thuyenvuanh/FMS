95fd465445c85ecfdbbc329d92002d2b40fb52ae
VienQuocBinh <realclonebinh@gmail.com>
2022-07-19 22:21:43 +0700

    Dashboard service

--- src/main/java/com/fptuni/fms/service/IDashboardService.java
+++ src/main/java/com/fptuni/fms/service/IDashboardService.java
@@ -1,10 +1,14 @@
 package com.fptuni.fms.service;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
 
-public interface IDashboardService {
-    String index(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+public interface IDashBoardService {
+    List<Date> getDateRange(HttpServletRequest request, HttpServletResponse response);
+
+    List<Date> getTimeRange(HttpServletRequest request, HttpServletResponse response);
+
 }
